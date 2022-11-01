dd783b91ae9ff33ab7303417d24b3529c0e447aa
Diksha Sharma <diksha@softobiz.com>
2022-11-01 15:31:53 +0530

    Changes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,42 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+const messages=[
+  {
+    text:"hi dear",
+    user:"Diksha",
+    added:new Date()
+  },
+  {
+    text:"hi",
+    user:"shubhangi",
+    added:new Date()
+  },
+  {
+    text:"Hiii",
+    user:"kunal",
+    added:new Date()
+    
+  },
+  {
+    text:"heloo",
+    user:"shravan",
+    added:new Date()
+  }
+]
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: "Minimessageboard", messages: messages });
 });
+router.get('/new',function(req,res){
+  res.render('form',{ title: "NewMessage"})
+})
+router.post('/new',function(req,res){
+ let user=req.body.user
+ let message=req.body.message
+ messages.push({text:message,user:user,added:new Date()})
+ res.redirect('/')
+})
+
 
 module.exports = router;
